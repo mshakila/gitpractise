447cf57437da1beae4ae326b189be2471fd09a7a
M <mshakila.she@gmail.com>
2024-05-10 09:16:03 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # simple operations
-def addition(a,b):
-    return a+b
+def addition(a,b,c):
+    print(f"Sum of {a+b+c}")
+    return a+b+c
 
 def multiplication(a,b):
     return a*b
 
 def subtract(m,n):
     return a-b
+
     
